15d40a4f173c94ce2296e332c0e133e179eaf3e6
Anderson Sousa <and3rsonsousa@outlook.com>
2023-03-29 12:15:24 -0300

    bg

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,10 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
 	return (
-		<div class="min-h-screen grid place-items-center">
+		<div
+			class="min-h-screen grid place-items-center"
+			style={{ backgroundImage: "url(/bg.jpg)" }}
+		>
 			<Slot />
 		</div>
 	);
