bfa6cce9524fc9b281d0b0d0556003bf480e22a4
Chico Sousa <ochicosousa@gmail.com>
2025-02-05 17:22:50 -0300

    estagi

--- src/routes/estagi/index.tsx
+++ src/routes/estagi/index.tsx
@@ -1,8 +1,5 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = (request) => {
-	throw request.redirect(
-		302,
-		`/whatsapp?source=${encodeURI("Vitinho Ceará")}`
-	);
+	throw request.redirect(302, `/whatsapp?source=${encodeURI("Estagiário")}`);
 };
